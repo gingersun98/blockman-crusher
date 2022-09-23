fad4ac5ce03ca70c45bfd9ca61968d84d51ff519
gingersun98 <gingersungames@gmail.com>
2022-09-23 21:32:47 +0700

    Update random loot, adjust ranged weapon & Boss movement, Show HP Crush Object

--- plugin/myplugin/entity/9efabf7f-63f2-43e2-afa8-a3c9c051257a/server/server_script.lua
+++ plugin/myplugin/entity/9efabf7f-63f2-43e2-afa8-a3c9c051257a/server/server_script.lua
@@ -1,12 +1,11 @@
 local randomList = { [1] = 'myplugin/15a5078e-f6aa-45f1-aba7-6f7b4ecefce0', [2] = 'myplugin/547b7be6-2cb9-4e37-9bc4-bd58f37fb3a1', [3] = 'myplugin/d9575c4c-95cd-44fe-ad35-b44d9b19d730' }
 
 
-
 Trigger.addHandler(this:cfg(), "ENTITY_DIE", function(context)
     local entity = context.obj1
     local pos = entity:getFrontPos(1)                                --Get the front coordinates
     local params = {
-        item = Item.CreateItem('myplugin/547b7be6-2cb9-4e37-9bc4-bd58f37fb3a1', 1),
+        item = Item.CreateItem( randomList[ math.random( #randomList ) ], 1),
         map = entity.map,
         pos = pos
     }
